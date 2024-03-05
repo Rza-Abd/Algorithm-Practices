38c0d8eb90c0af1424e747b32af6e00d07c52c41
RezaAbedy <reza.abedy75@gmail.com>
2024-03-05 12:29:06 +0330

    Day_7_2

--- 3sum_Closest.py
+++ 3sum_Closest.py
@@ -4,25 +4,28 @@
 # Return the sum of the three integers.
 # I do this at 1401/12/15
 
-def threeSumClosest(nums: list[int], target: int) -> int:
+def threeSumClosest(nums, target):
+  
     nums.sort()
     n = len(nums)
-    min_diff = float('inf')
-    res = 0
+    best = sum(nums[:3])
+
     for i in range(n):
-        l, r = i + 1, n - 1
-        while l < r:
-            s = nums[i] + nums[l] + nums[r]
-            if abs(s - target) < min_diff:
-                min_diff = abs(s - target)
-                res = s
-            if s < target:
-                l += 1
-            else:
-                r -= 1
-    return s
+        lo = i+1
+        hi = n-1
+        while lo < hi:
+        current_sum = nums[i] + nums[lo] + nums[hi]
+        if abs(current_sum - target) < abs(best - target):
+            best = current_sum
+        if current_sum < target:
+            lo += 1  
+        else: 
+            hi -= 1
+
+    return best
+
 
 # Test the code
-nums = [-1,2,1,-4]
-target = 1
+nums = [1,1,1,0]
+target = -100
 print(threeSumClosest(nums, target))
